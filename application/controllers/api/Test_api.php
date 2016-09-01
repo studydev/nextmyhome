857c6a3fc36b2b874a9ae16a33fedb734bb1a152
김현수 <meelong0@gmail.com>
2016-09-02 00:39:08 +0900

    TestApi 추가

---
+++ application/controllers/api/Test_api.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * User: 김현수
+ * Date: 2016-09-01
+ * Time: 오전 9:22
+ */
+
+class Test_api extends CI_Controller {
+
+    public function index()
+    {
+        echo "hi";
+    }
+}
